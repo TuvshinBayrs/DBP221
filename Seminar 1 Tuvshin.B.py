4c2ce82af350918af8816f6888d88129d215a25f
TuvshinBayrs <b20fa1516@ufe.edu.mn>
2022-02-23 16:55:06 +0800

    Seminar 2 oruulav.

--- Seminar 1 Tuvshin.B.py
+++ Seminar 1 Tuvshin.B.py
@@ -1,6 +1,8 @@
-#1
-#lists = ['python','php','java']
-#print(lists[0:3])
+# #1
+# lists = ['python','php','java']
+# print(lists[0:3])
+a=3
+print(a)
 
 #2
 #num = [5,8,2,1,3,4,5,10,19,20]
---
+++ Seminar 2 Tuvshin.B.py
@@ -0,0 +1,85 @@
+#1
+# a = int(input())
+# l = a
+# k = 0
+# while a > 0:
+#     digit1 = a%10
+#     k = k*10 + digit1
+#     a = a // 10 
+# if l == k:
+#     print('Palindrome')
+# else :
+#     print('It is not')
+
+#2
+# m = list(input())
+# def count(a):
+#     k = 0
+#     c = 0
+#     for i in range(len(a)):
+#         if a[i] >= 'a' and a[i] <= 'z':
+#             k +=1
+#             i +=1
+#         elif (a[i] >= 'A' and a[i] <= 'Z'):
+#             c +=1
+#             i +=1
+#     print('Capital letter', c)
+#     print('Lowercase letter', k)
+# count(m)    
+
+#3
+# a = [1, 2, 3, 4, 5, 6]
+# def multiply(x):
+#     k = 1
+#     for i in x:
+#         k = k * i
+#         i += 1 
+#     print(k)
+# multiply(a)
+
+#4
+# n = int(input())
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     elif n == 0:
+#         return 1
+#     elif n < 0:
+#         return 'bolomjgui'
+#     else: 
+#         return n * factorial(n - 1)
+# factorial(n)
+
+#5
+# a = [2 , 2, 3, 4]
+# def rev(str):
+#     str.reverse()
+#     print(str)
+# rev(a)
+
+#6
+# list1 = [1 , 3, 7, 8, 10]
+# def sum1(list):
+#     k = 0
+#     for v in list:
+#         k += v
+#         v += 1
+#     print(k)
+    
+# sum1(list1)
+
+#7
+# a = [1 , 2, 2, 2, 3, 4, 5, 6, 6]
+# def clear(l):
+#     s = set(l)
+#     return s
+# clear(a)
+
+#8
+# list = [111, 134, 231]
+# def sum3(num):
+#     return max(num)
+    
+        
+# sum3(list)
+        
