72227f69152a27a356a62454dda9ce7d09a46701
TuvshinBayrs <b20fa1516@ufe.edu.mn>
2022-03-15 19:46:46 +0800

    Seminar 5 oruulav.

---
+++ Seminar 5 TuvshinB.py
@@ -0,0 +1,30 @@
+# 1
+# n = int(input())
+# for i in range(n + 1):
+#     k = "*"*i
+#     i += 1
+#     print(k)
+# 2
+# n = int(input())
+# for i in range(n + 1):
+#     k = "*"*i
+#     i +=1
+#     print([k])
+# 3
+# students = {
+# 'Bat': 18, 'Oyun': 22, 'Dulam': 21, 'Suren': 20
+# }
+# list = []
+# for key, value in students.items():
+#     if value == max(students.values()) or value == min(students.values()):
+#         list.append(key)
+#         list.reverse()     
+# print(tuple(list))  
+# 4  
+# import numpy as np
+# arr = np.arange(1, 1001)
+# sum = 0
+# for i in arr:
+#     if i % 3 == 0 or i % 7 == 0:
+#         sum += i 
+# print(sum)
